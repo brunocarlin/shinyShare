7360bea2ee9f3edb4e0b345c335ea9f3a39e2e71
runner <runner@Mac-1656.local>
2020-07-12 21:36:38 +0000

    Style

--- R/share_message_twitter.R
+++ R/share_message_twitter.R
@@ -14,7 +14,7 @@ share_message_twitter <- function(text, url, hashtags) {
     "https://twitter.com/intent/tweet?text=",
     utils::URLencode(paste0(text, "\n")),
     "&url=",
-    utils::URLencode(paste0(url, "\n")),"&hashtags=",
+    utils::URLencode(paste0(url, "\n")), "&hashtags=",
     utils::URLencode(hashtags)
   )
 }
